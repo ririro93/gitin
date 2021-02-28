9c886dfbabb9e09b3ef930fc3289b30e06a149e4
Junha Moon <ririro93@gmail.com>
2021-02-28 16:38:02 +0900

    Fix: allauth bug due to db wipe

--- gitin/users/admin.py
+++ gitin/users/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.sites.models import Site
+
+admin.site.unregister(Site)
+
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    fields = ('id', 'name', 'domain')
+    readonly_fields = ('id',)
+    list_display = ('id', 'name', 'domain')
+    list_display_links = ('name',)
+    search_fields = ('name', 'domain')
+    
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
@@ -8,4 +21,3 @@ class CustomUserAdmin(admin.ModelAdmin):
         'username',
         'date_joined',
     )
-   
