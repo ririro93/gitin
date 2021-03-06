895651ce465f1c95118a934a8c92b0a96a28112d
Junha Moon <ririro93@gmail.com>
2021-03-06 16:10:14 +0900

    Test: how sha changes on filename change

---
+++ test/sha_after_namechange.py
@@ -0,0 +1,4 @@
+# will changing filename change the sha of this file on git?
+# original sha : d30f20882dcaacd8fd2ecf14eba97ea1941c7aa7
+
+# after name change
--- test/sha_test.py
+++
@@ -1,2 +0,0 @@
-# will changing filename change the sha of this file on git?
-# original sha : 
